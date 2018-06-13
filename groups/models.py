b110dd2cfd8294ae3d8b5e2b79404b79720fcf64
Untitled <john.d.reddock@gmail.com>
2018-06-13 15:20:31 -0400

    groups model

---
+++ groups/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.text import slugify
+# GROUPS MODELS.PY FILE
+import misaka
+
+import django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
+
+class Group(models.Model):
+    pass
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group,related_name='memberships')
+    user = models.ForeignKey(User,related_name='user_groups')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group','user')
+
+
+
+
+
