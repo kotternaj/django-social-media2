db9e48a183489f22c0c45b13b0ae86529bdfdfcb
Untitled <john.d.reddock@gmail.com>
2018-06-12 20:09:43 -0400

    urls and base navbar

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts',
 ]
 
@@ -119,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.HomePage.as_view,nam='home'),
+    url(r'^$',views.HomePage.as_view(),name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'accounts/',include('accounts.urls',namespace='accounts')),
+    url(r'accounts/',include('django.contrib.auth.urls')),
 ]
